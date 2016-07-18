605e10c023c5f69dde943b90716e551dbe1a1b2c
Samuele Fiorini <samuele.fiorini@gmail.com>
2016-07-18 14:31:46 +0200

    PEP8 compliance improved

--- SDG4ML/core/strategies.py
+++ SDG4ML/core/strategies.py
@@ -1,23 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-    This module implements several synthetic strategies for the generation of synthetic regression problems.
+    Strategies for synthetic regression problems generation.
 
-    - NULL: regression problem with no signal, random labels uncorrelated with data.
-    - SPARSE: sparse linear regression problem y = univ_random_X*sparse_beta + noise.
-    - CORRELATED: linearly correlated regression problem y = multiv_random_X*sparse_beta + noise.
-    - BLOCK CORRELATED: linearly block correlated regression problem y = multiv_random_X*block_sparse_beta + noise.
+    IMPLEMTENTED STRATEGIES:
+
+    - NULL: regression problem with no signal, random labels uncorrelated with
+            data.
+    - SPARSE: sparse linear regression problem
+              y = univ_random_X*sparse_beta + noise.
+    - CORRELATED: linearly correlated regression problem
+              y = multiv_random_X*sparse_beta + noise.
+    - BLOCK CORRELATED: linearly block correlated regression problem
+              y = multiv_random_X*block_sparse_beta + noise.
     - ZOU HASTIE 2005d: regression problem as in Zou Hastie 2005 (d) scenario.
-
+    - MULTIVARIATE GROUPS: classification problem, each point is sampled from
+                           a multivariate normal distribution.
 
     REF:
-    Zou and Hastie (2005), Regularization and variable selection via the elastic net.
+
+    Zou and Hastie (2005), Regularization and variable selection via the
+    elastic net.
 """
 from __future__ import division
 import numpy as np
 
+
 def null(n=100, d=150, normalized=False, seed=None, **kwargs):
-    """Generate a signal-less {X,Y} dataset with data and labels completely uncorrelated.
+    """Generate a signal-less {X,Y} dataset with data and labels uncorrelated.
 
     Parameters
     ----------
@@ -26,7 +36,8 @@ def null(n=100, d=150, normalized=False, seed=None, **kwargs):
     d : int, optional (default is `150`)
         number of dimensions
     normalized : bool, optional (default is `False`)
-        if normalized is true than the data matrix is normalized as data/sqrt(n)
+        if normalized is true than the data matrix is normalized as
+        data/sqrt(n)
     seed : float, optional (default is `None`)
         random seed initialization
 
@@ -46,21 +57,25 @@ def null(n=100, d=150, normalized=False, seed=None, **kwargs):
     else:
         factor = 1
 
-    X = np.random.randn(n,d)/factor # Generate random data
-    Y = -1 + 2*np.random.randn(n,1) # Generate random labels
+    X = np.random.randn(n, d)/factor  # Generate random data
+    Y = -1 + 2*np.random.randn(n, 1)  # Generate random labels
 
-    if seed is not None: # restore random seed
+    if seed is not None:  # restore random seed
         np.random.set_state(state0)
 
     return X, Y, np.zeros(d)
 
 
-def sparse(n=100, d=150, k=15, amplitude=3.5, sigma=0.5, normalized=False, seed=None, **kwargs):
-    """Generate a sparse linear regression (X,Y) problem. The relationship between input and output is given by:
+def sparse(n=100, d=150, k=15, amplitude=3.5, sigma=0.5, normalized=False,
+           seed=None, **kwargs):
+    """Generate a sparse linear regression (X,Y) problem.
+
+    The relationship between input and output is given by:
 
                                             Y = X*beta + noise
 
-    where X ~ N(0,1), beta is sparse and the nonzero values are in {+-amplitude}, noise ~ N(0,sigma).
+    where X ~ N(0,1), beta is sparse and the nonzero values are in
+    {+-amplitude}, noise ~ N(0,sigma).
 
     Parameters
     ----------
@@ -75,7 +90,8 @@ def sparse(n=100, d=150, k=15, amplitude=3.5, sigma=0.5, normalized=False, seed=
     sigma : float, optional (default is `0.5`)
         Gaussian noise std
     normalized : bool, optional (default is `False`)
-        if normalized is true than the data matrix is normalized as data/sqrt(n)
+        if normalized is true than the data matrix is normalized as
+        data/sqrt(n)
     seed : float, optional (default is `None`)
         random seed initialization
 
@@ -97,27 +113,32 @@ def sparse(n=100, d=150, k=15, amplitude=3.5, sigma=0.5, normalized=False, seed=
     else:
         factor = 1
 
-    X = np.random.randn(n,d)/factor            # generate random data
+    X = np.random.randn(n, d)/factor            # generate random data
 
-    beta = np.zeros(d)                         # init beta vector
-    S0 = np.random.choice(d, k, replace=False) # extract k indexes from d
-    beta[S0] = amplitude                       # set them to amplitude
-    beta *= np.sign(np.random.randn(d))        # with random sign
+    beta = np.zeros(d)                           # init beta vector
+    S0 = np.random.choice(d, k, replace=False)   # extract k indexes from d
+    beta[S0] = amplitude                         # set them to amplitude
+    beta *= np.sign(np.random.randn(d))          # with random sign
 
-    Y = X.dot(beta) + sigma * np.random.randn(n) # evaluate labels
+    Y = X.dot(beta) + sigma * np.random.randn(n)  # evaluate labels
 
-    if seed is not None: # restore random seed
+    if seed is not None:  # restore random seed
         np.random.set_state(state0)
 
     return X, Y, beta
 
 
-def correlated(n=100, d=150, k=15, rho=0.5, amplitude=3.5, sigma=0.5, normalized=False, seed=None, **kwargs):
-    """Generate a linear regression (X,Y) problem. The relationship between input and output is given by:
+def correlated(n=100, d=150, k=15, rho=0.5, amplitude=3.5, sigma=0.5,
+               normalized=False, seed=None, **kwargs):
+    """Generate a linear regression (X,Y) problem.
+
+    The relationship between input and output is given by:
 
                                             Y = X*beta + noise
 
-    where X ~ multi variate random Gaussian (0, THETA), THETA is a (d,d) matrix that has THETA[j,k] = rho^abs(j-k), beta is sparse and the nonzero values are in {+-amplitude}, noise ~ N(0,sigma).
+    where X ~ multi variate random Gaussian (0, THETA), THETA is a (d,d)
+    matrix that has THETA[j,k] = rho^abs(j-k), beta is sparse and the nonzero
+    values are in {+-amplitude}, noise ~ N(0,sigma).
 
     Parameters
     ----------
@@ -134,7 +155,8 @@ def correlated(n=100, d=150, k=15, rho=0.5, amplitude=3.5, sigma=0.5, normalized
     sigma : float, optional (default is `0.5`)
         Gaussian noise std
     normalized : bool, optional (default is `False`)
-        if normalized is true than the data matrix is normalized as data/sqrt(n)
+        if normalized is true than the data matrix is normalized as
+        data/sqrt(n)
     seed : float, optional (default is `None`)
         random seed initialization
 
@@ -157,32 +179,39 @@ def correlated(n=100, d=150, k=15, rho=0.5, amplitude=3.5, sigma=0.5, normalized
         factor = 1
 
     # Create covariance matrix
-    THETA = np.zeros((d,d))
+    THETA = np.zeros((d, d))
     for i in range(d):
         for j in range(d):
-            THETA[i,j] = rho**np.abs(i-j)
+            THETA[i, j] = rho**np.abs(i-j)
 
-    X = np.random.multivariate_normal(mean=np.zeros(d), cov=THETA, size=(n))/factor
+    X = np.random.multivariate_normal(mean=np.zeros(d), cov=THETA,
+                                      size=(n))/factor
 
-    beta = np.zeros(d)                         # init beta vector
-    S0 = np.random.choice(d, k, replace=False) # extract k indexes from d
-    beta[S0] = amplitude                       # set them to amplitude
-    beta *= np.sign(np.random.randn(d))        # with random sign
+    beta = np.zeros(d)                          # init beta vector
+    S0 = np.random.choice(d, k, replace=False)  # extract k indexes from d
+    beta[S0] = amplitude                        # set them to amplitude
+    beta *= np.sign(np.random.randn(d))         # with random sign
 
-    Y = X.dot(beta) + sigma * np.random.randn(n) # evaluate labels
+    Y = X.dot(beta) + sigma * np.random.randn(n)  # evaluate labels
 
-    if seed is not None: # restore random seed
+    if seed is not None:  # restore random seed
         np.random.set_state(state0)
 
     return X, Y, beta
 
 
-def block_correlated(n=100, d=150, k=15, rho=0.5, amplitude=3.5, sigma=0.5, normalized=False, seed=None, **kwargs):
-    """Generate a linear regression (X,Y) problem. The relationship between input and output is given by:
+def block_correlated(n=100, d=150, k=15, rho=0.5, amplitude=3.5, sigma=0.5,
+                     normalized=False, seed=None, **kwargs):
+    """Generate a linear regression (X,Y) problem.
+
+    The relationship between input and output is given by:
 
                                             Y = X*beta + noise
 
-    where X is made by stacking on the rows a multi variate random Gaussian (0, THETA) of shape (n,k) with a single variate random Gaussian of shape (n,d-k), THETA is a (d,d) matrix that has THETA[j,k] = rho^abs(j-k), beta is sparse and the nonzero values are in {+-amplitude}, noise ~ N(0,sigma).
+    where X is made by stacking on the rows a multi variate random Gaussian
+    (0, THETA) of shape (n,k) with a single variate random Gaussian of shape
+    (n,d-k), THETA is a (d,d) matrix that has THETA[j,k] = rho^abs(j-k), beta
+    is sparse and the nonzero values are in {+-amplitude}, noise ~ N(0,sigma).
 
     Parameters
     ----------
@@ -199,7 +228,8 @@ def block_correlated(n=100, d=150, k=15, rho=0.5, amplitude=3.5, sigma=0.5, norm
     sigma : float, optional (default is `0.5`)
         Gaussian noise std
     normalized : bool, optional (default is `False`)
-        if normalized is true than the data matrix is normalized as data/sqrt(n)
+        if normalized is true than the data matrix is normalized as
+        data/sqrt(n)
     seed : float, optional (default is `None`)
         random seed initialization
 
@@ -222,30 +252,34 @@ def block_correlated(n=100, d=150, k=15, rho=0.5, amplitude=3.5, sigma=0.5, norm
         factor = 1
 
     # Create covariance matrix
-    THETA = np.zeros((k,k))
+    THETA = np.zeros((k, k))
     for i in range(k):
         for j in range(k):
-            THETA[i,j] = rho**np.abs(i-j)
+            THETA[i, j] = rho**np.abs(i-j)
 
-    Xleft = np.random.multivariate_normal(mean=np.zeros(k), cov=THETA, size=(n))
-    Xright = np.random.randn(n,d-k)
-    X = np.hstack((Xleft,Xright))/factor
+    Xleft = np.random.multivariate_normal(mean=np.zeros(k), cov=THETA,
+                                          size=(n))
+    Xright = np.random.randn(n, d-k)
+    X = np.hstack((Xleft, Xright))/factor
 
-    beta = np.zeros(d)                         # init beta vector
-    S0 = np.random.choice(d, k, replace=False) # extract k indexes from d
-    beta[S0] = amplitude                       # set them to amplitude
-    beta *= np.sign(np.random.randn(d))        # with random sign
+    beta = np.zeros(d)                           # init beta vector
+    S0 = np.random.choice(d, k, replace=False)   # extract k indexes from d
+    beta[S0] = amplitude                         # set them to amplitude
+    beta *= np.sign(np.random.randn(d))          # with random sign
 
-    Y = X.dot(beta) + sigma * np.random.randn(n) # evaluate labels
+    Y = X.dot(beta) + sigma * np.random.randn(n)   # evaluate labels
 
-    if seed is not None: # restore random seed
+    if seed is not None:   # restore random seed
         np.random.set_state(state0)
 
     return X, Y, beta
 
 
-def zou_hastie_2005d(n=100, d=150, k=15, amplitude=3.5, sigma=0.5, normalized=False, seed=None, **kwargs):
-    """Generate a linear regression problem (X,Y) as in Zou Hastie 2005 scenario (d).The relationship between input and output is given by:
+def zou_hastie_2005d(n=100, d=150, k=15, amplitude=3.5, sigma=0.5,
+                     normalized=False, seed=None, **kwargs):
+    """Generate a linear regression problem (X,Y) as in Zou Hastie 2005 (d).
+
+    The relationship between input and output is given by:
 
                                             Y = X*beta + noise
 
@@ -268,13 +302,16 @@ def zou_hastie_2005d(n=100, d=150, k=15, amplitude=3.5, sigma=0.5, normalized=Fa
      d : int, optional (default is `150`)
          total number of dimensions
      k : int, optional (default is `15`)
-         total number of features with coefficient different from zero (k must be integer multiple of 3. If it is not, then k is forced to k - mod(k,3)).
+         total number of features with coefficient different from zero (k must
+         be integer multiple of 3. If it is not, then k is forced to
+         k - mod(k,3)).
      amplitude : float,  optional (default is `3.5`)
          amplitude of the generative linear model
      sigma : float, optional (default is `0.5`)
          Gaussian noise std
      normalized : bool, optional (default is `False`)
-         if normalized is true than the data matrix is normalized as data/sqrt(n)
+         if normalized is true than the data matrix is normalized as
+         data/sqrt(n)
      seed : float, optional (default is `None`)
          random seed initialization
 
@@ -297,35 +334,38 @@ def zou_hastie_2005d(n=100, d=150, k=15, amplitude=3.5, sigma=0.5, normalized=Fa
         factor = 1
 
     # check if k is integer multiple of 3
-    if k%3 != 0: k -= k%3
+    if k % 3 != 0:
+        k -= k % 3
 
     # Generate groups
-    Z = np.random.randn(n,3)
-    G1 = np.array([Z[:,0],]*(k//3)).T + 0.001 * np.random.randn(n,k//3)
-    G2 = np.array([Z[:,1],]*(k//3)).T + 0.001 * np.random.randn(n,k//3)
-    G3 = np.array([Z[:,2],]*(k//3)).T + 0.001 * np.random.randn(n,k//3)
-    G4 = np.random.randn(n,d-k)
-    X = np.hstack((G1,G2,G3,G4))/factor
+    Z = np.random.randn(n, 3)
+    G1 = np.array([Z[:, 0], ]*(k//3)).T + 0.001 * np.random.randn(n, k//3)
+    G2 = np.array([Z[:, 1], ]*(k//3)).T + 0.001 * np.random.randn(n, k//3)
+    G3 = np.array([Z[:, 2], ]*(k//3)).T + 0.001 * np.random.randn(n, k//3)
+    G4 = np.random.randn(n, d-k)
+    X = np.hstack((G1, G2, G3, G4))/factor
 
-    beta = np.zeros(d) # init beta vector
+    beta = np.zeros(d)   # init beta vector
     beta[:k] = amplitude
-    beta *= np.sign(np.random.randn(d)) # with random sign
+    beta *= np.sign(np.random.randn(d))   # with random sign
 
-    Y = X.dot(beta) + sigma * np.random.randn(n) # evaluate labels
+    Y = X.dot(beta) + sigma * np.random.randn(n)   # evaluate labels
 
-    if seed is not None: # restore random seed
+    if seed is not None:  # restore random seed
         np.random.set_state(state0)
 
     return X, Y, beta
 
 
 def wrapper(strategy='sparse', **kwargs):
-    """Wrapper that let you generate a regression problem according to several strategies (see docstrings for further details).
+    """Wrapper that let you generate a regression problem according to several
+    strategies (see docstrings for further details).
 
     Parameters
     ----------
     strategy : str, optional (default is `sparse`)
-        data generation strategy, this can be either 'null', 'sparse', 'correlated', 'block_correlated' or 'zou_hastie_2005d'.
+        data generation strategy, this can be either 'null', 'sparse',
+        'correlated', 'block_correlated' or 'zou_hastie_2005d'.
     """
     if strategy.lower() == 'null':
         return null(**kwargs)
@@ -338,4 +378,6 @@ def wrapper(strategy='sparse', **kwargs):
     elif strategy.lower() == 'zou_hastie_2005d':
         return zou_hastie_2005d(**kwargs)
     else:
-        print("{} is not a valid strategy. Accepted strategies are: 'null', 'sparse', 'correlated', 'block_correlated' or 'zou_hastie_2005d'.")
+        print("{} is not a valid strategy. Accepted strategies are: 'null', \
+              'sparse', 'correlated', 'block_correlated' or \
+              'zou_hastie_2005d'.")
